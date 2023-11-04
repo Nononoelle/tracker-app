59c44d1208c3106e7cac2cca4f4b314373762890
Nononoelle <81425782+Nononoelle@users.noreply.github.com>
2023-11-03 21:19:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ def load_data():
 
 
 # 3. save file
-# def save_file():
-#     df.to_excel('Tracker_dataset_new.xlsx', index=False)
+def save_file():
+    df.to_excel('Tracker_dataset_new.xlsx', index=False)
 
 # 4. page layout
 ## Left sidebar
@@ -31,7 +31,8 @@ with st.container():
         try:
             df_upload = pd.read_excel(uploaded_file)
             df_upload["Employee ID"] = df_upload["Employee ID"].str.capitalize()
-            df_upload = df_upload[["Employee ID", "New Key Role"]]
+            df_upload = df_upload[["Employee ID", "Key Role"]]
+            df_upload.rename(columns={"Key Role":"New Key Role"}, inplace=True)
             df_main = load_data()
             df = pd.merge(df_main, df_upload, on="Employee ID", how="left")
             for i in df_upload["Employee ID"]:
@@ -64,15 +65,16 @@ with st.container():
                 st.write(filtered_data)
 
             # save button
-            # button_clicked = st.button("Save", key="save_file")
-            # button_executed = False
+            button_clicked = st.button("Save", key="save_file")
+            button_executed = False
 
-            # if button_clicked:
-            #     save_file()
-            #     button_executed = True
+            if button_clicked:
+                save_file()
+                button_executed = True
 
-            # if button_executed:
-            #     st.write("The File has been updated and saved")
+            if button_executed:
+                st.write("The File has been updated and saved")
+                
             # bar chart on key role
             st.subheader("Key Role Distribution")
             category_columns = ["Country", "Business Line", "Department"]
